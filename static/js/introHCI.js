0258c9e7dcba9aedebcb29ead543044a74c0e06e
Yu Feng <yuf107@ucsd.edu>
2018-01-24 11:05:45 -0800

    Created_Heroku_app

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,28 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript has taken control");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e){
+	e.preventDefault();
+	var containingProject = $(this).closest(".project");
+    	//containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+
+	var containingProject = $(this).closest(".project");
+    	var description = $(containingProject).find(".project-description");
+    	if (description.length == 0) {
+       		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    	} else {
+       		// description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    
+		$(".project-description").fadeOut();	
+	}
 }
